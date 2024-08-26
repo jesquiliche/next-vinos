2c14d44580bf887dffe7ee2b699e918017a34dcf
Jesús Quintana <jesquiliche@hotmail.comm>
2024-08-26 20:33:00 +0200

    Arreglado estado Zustand

--- src/components/products/productView.tsx
+++ src/components/products/productView.tsx
@@ -1,7 +1,8 @@
+'use client'
 import React from "react";
 import { Producto } from "@/app/interfaces/Product";
 import Link from "next/link";
-import { titleFont } from "@/config/fonts";
+import useCartStore from "@/store/useCartstore"; // Asegúrate de usar la ruta correcta
 
 const ProductView = ({
   id,
@@ -11,44 +12,53 @@ const ProductView = ({
   graduacion,
   ano,
 }: Producto) => {
-  return (
-    <>
-      <div
-        key={id.toString()}
-        className="h-full border-gray-400 border rounded-md p-4 flex flex-col justify-between"
-      >
-        <p className="text-shadow-title text-3xl font-bold italic text-red-600">
-          {precio.toString()} €
-        </p>
-        <div>
-          <img
-            src={imagen}
-            alt={nombre}
-            width={100}
-            height={100}
-            className="mx-auto"
-          />
-        </div>
-        <div>
-          <h2
-            className={`text-lg font-bold text-center truncate`}
-          >
-            {nombre}
-          </h2>
+  const addToCart = useCartStore((state) => state.addToCart);
 
-          <p>Graduación: {graduacion.toString()}</p>
-          <p>Año: {ano}</p>
-        </div>
-        <div className="flex space-x-2">
-          <Link href={`/product/${id.toString()}`} className="btn-primary">
-            Ver
-          </Link>
-          <button className="btn-primary">
-            Añadir
-          </button>
-        </div>
+  const handleAddToCart = () => {
+    const productToAdd = {
+      id:Number(id),
+      nombre,
+      precio:Number(precio),
+      imagen,
+    };
+
+    addToCart(productToAdd);
+  };
+
+  return (
+    <div
+      key={id.toString()}
+      className="h-full border-gray-400 border rounded-md p-4 flex flex-col justify-between"
+    >
+      <p className="text-shadow-title text-3xl font-bold italic text-red-600">
+        {precio.toString()} €
+      </p>
+      <div>
+        <img
+          src={imagen}
+          alt={nombre}
+          width={100}
+          height={100}
+          className="mx-auto"
+        />
       </div>
-    </>
+      <div>
+        <h2 className="text-lg font-bold text-center truncate">
+          {nombre}
+        </h2>
+
+        <p>Graduación: {graduacion.toString()}</p>
+        <p>Año: {ano}</p>
+      </div>
+      <div className="flex space-x-2">
+        <Link href={`/product/${id.toString()}`} className="btn-primary">
+          Ver
+        </Link>
+        <button className="btn-primary" onClick={handleAddToCart}>
+          Añadir
+        </button>
+      </div>
+    </div>
   );
 };
 
